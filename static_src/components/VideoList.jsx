f10f6d4712a91a1d899e75143fd767dcef69e63d
Alexander Snorkin <snorkin@phystech.edu>
2018-03-04 17:54:53 +0300

    Сделал поиск

--- static_src/components/VideoList.jsx
+++ static_src/components/VideoList.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { List } from 'material-ui/List';
+import PropTypes from 'prop-types';
 import VideoListItem from './VideoListItem';
-
-const VIDEOS = ['Видео 1', 'Видео 2', 'Видео 3'];
+import CircularProgress from 'material-ui/CircularProgress';
 
 class VideoList extends React.Component {
-    render() {
-        const videoList = VIDEOS.map(item => <VideoListItem name={ item } />);
+    static propTypes = {
+        videoList: PropTypes.arrayOf(PropTypes.shape(VideoListItem.propTypes)),
+        isLoading: PropTypes.bool,
+        fetchVideo: PropTypes.func.isRequired,
+    };
 
+    static defaultProps = {
+        videoList: [],
+        isLoading: false,
+    };
+
+    render() {
+        const videoList = this.props.videoList.map(item =>
+            <VideoListItem name={ item.name } fetchVideo={ this.props.fetchVideo } />);
+        if (this.props.isLoading) {
+            return (
+                <CircularProgress />
+            );
+        }
         return (
             <div className="video-list">
                 <List>{ videoList }</List>
--- static_src/components/VideoListItem.jsx
+++ static_src/components/VideoListItem.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { ListItem } from 'material-ui/List';
 import PropTypes from 'prop-types';
+import apiUrls from '../constants/apiUrls';
 
 
 class VideoListItem extends React.Component {
     static propTypes = {
-        name: PropTypes.string.isRequired,
+        name: PropTypes.string,
+        author: PropTypes.string,
+        description: PropTypes.string,
+        fetchVideo: PropTypes.func.isRequired,
+    };
+
+    static defaultProps = {
+        name: '',
+        author: '',
+        description: '',
+    };
+
+    handleClick = (e) => {
+        const url = `${apiUrls.video}?name=${e.target.innerHTML}`;
+        this.props.fetchVideo(url);
     };
 
     render() {
         return (
-            <ListItem className="video-list-item" primaryText={ this.props.name } />
+            <ListItem className="video-list-item" primaryText={ this.props.name } onClick={ this.handleClick } />
         );
     }
 }
